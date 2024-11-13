8a60a795a7ca195a671aaaed454f5933a6be12bb
Chathura Ranasinghe <chathurar.99.bc@gmail.com>
2024-11-14 03:21:35 +0530

    preload

--- src/components/Interact/Loading.tsx
+++ src/components/Interact/Loading.tsx
@@ -1,14 +1,14 @@
-import loading from '@/assets/images/loading.gif'
+import loading from '@/assets/images/earth.gif'
 
 export default function Loading() {
   return (
-    <div className="fixed flex top-0 left-0 h-screen w-full items-center justify-center bg-muted z-20">
+    <div className="fixed flex flex-col top-0 left-0 h-screen w-full items-center justify-center bg-muted z-20">
         <img
           src={loading}
           alt='loading'
-          className='h-40 w-40'
+          className='h-36 w-36 dark:grayscale '
         />
-        <span className='absolute font-bold'>Loading...</span>
+        <span className='font-bold text-xl'>Loading...</span>
     </div>
   )
 }
